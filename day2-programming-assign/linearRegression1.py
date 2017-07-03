7515e2f42cab28ef5d0d5e54b4c2fd8bb946593c
Kaushal Kishore <kshr4kshl@gmail.com>
2017-07-03 12:06:12 +0530

    Add Command-Line arguments Support

--- day2-programming-assign/linearRegression1.py
+++ day2-programming-assign/linearRegression1.py
@@ -1,41 +1,44 @@
 #!/usr/bin/python3
-
+from sys import argv
 import numpy as np
 from numpy.linalg import inv
 import matplotlib.pyplot as plt
 
-fname = 'dat1.txt'
-data = np.loadtxt(fname)
-x = data[:,0]
-y = data[:,1]
+if len(argv)!= 2:
+        print("Usage: argv[0] <file_name> ")
+else:
+    fname = str(argv[1])
+    data = np.loadtxt(fname)
+    x = data[:,0]
+    y = data[:,1]
 
-n = int(len(x))
-sum_xi = x.sum()
-sum_xi_sqr = np.square(x).sum()
-sum_yi = y.sum()
-sum_xi_yi = (x*y).sum()
+    n = int(len(x))
+    sum_xi = x.sum()
+    sum_xi_sqr = np.square(x).sum()
+    sum_yi = y.sum()
+    sum_xi_yi = (x*y).sum()
 
-weights = np.array([0 , 0])
+    weights = np.array([0 , 0])
 
-A = np.array([[n, sum_xi], [sum_xi , sum_xi_sqr]])
-B = np.array([sum_yi , sum_xi_yi])
-inv_matrices = inv(A)
-weights = inv_matrices.dot(B)
-[c , m ] = weights
-# y = mx + c
-print("SLOPE: ", m, "INTERCEPT: ", c)
+    A = np.array([[n, sum_xi], [sum_xi , sum_xi_sqr]])
+    B = np.array([sum_yi , sum_xi_yi])
+    inv_matrices = inv(A)
+    weights = inv_matrices.dot(B)
+    [c , m ] = weights
+    # y = mx + c
+    print("SLOPE: ", m, "INTERCEPT: ", c)
 
-X = np.array([np.min(x), np.max(x)])
-Y = np.zeros(X.shape)
-Y[0] = m*X[0]+c
-Y[1] = m*X[1]+c
+    X = np.array([np.min(x), np.max(x)])
+    Y = np.zeros(X.shape)
+    Y[0] = m*X[0]+c
+    Y[1] = m*X[1]+c
 
-plt.scatter(x,y)
-plt.figure(1)
-plt.plot(X,Y,'b-')
-plt.show()
+    plt.scatter(x,y)
+    plt.figure(1)
+    plt.plot(X,Y,'b-')
+    plt.show()
 
-fout = open('dat1.answer' , 'w')
-result = "SLOPE: "+str(m)+"\nINTERCEPT: "+str(c)+"\n"
-fout.write(result)
-fout.close()
+    fout = open('dat1.answer' , 'w')
+    result = "SLOPE: "+str(m)+"\nINTERCEPT: "+str(c)+"\n"
+    fout.write(result)
+    fout.close()
--- day2-programming-assign/linearRegression2.py
+++ day2-programming-assign/linearRegression2.py
@@ -1,43 +1,47 @@
 #!/usr/bin/python3
 
+from sys import argv
 import numpy as np
 from numpy.linalg import inv
 import matplotlib.pyplot as plt
 
-fname = 'dat2.txt'
-data = np.loadtxt(fname)
-y = data[:]
-x = np.arange(1,len(data)+1)
+if len(argv)!= 2:
+        print("Usage: argv[0] <file_name> ")
+else:
+    fname = str(argv[1])
+    data = np.loadtxt(fname)
+    y = data[:]
+    x = np.arange(1,len(data)+1)
 
-n = int(len(x))
-sum_xi = x.sum()
-sum_xi_sqr = np.square(x).sum()
-sum_yi = y.sum()
-sum_xi_yi = (x*y).sum()
+    n = int(len(x))
+    sum_xi = x.sum()
+    sum_xi_sqr = np.square(x).sum()
+    sum_yi = y.sum()
+    sum_xi_yi = (x*y).sum()
 
-weights = np.array([0 , 0])
-
-
-A = np.array([[n, sum_xi], [sum_xi , sum_xi_sqr]])
-B = np.array([sum_yi , sum_xi_yi])
-inv_matrices = inv(A)
-weights = inv_matrices.dot(B)
-[c , m ] = weights
-# y = mx + c
-print("SLOPE: ", m, "INTERCEPT: ", c)
-
-X = np.array([np.min(x), np.max(x)])
-Y = np.zeros(X.shape)
-Y[0] = m*X[0]+c
-Y[1] = m*X[1]+c
-
-plt.scatter(x,y)
-plt.figure(1)
-plt.plot(X,Y,'b-')
-plt.show()
+    weights = np.array([0 , 0])
 
 
-fout = open('dat2.answer' , 'w')
-result = "SLOPE: "+str(m)+"\nINTERCEPT: "+str(c)+"\n"
-fout.write(result)
-fout.close()
+    A = np.array([[n, sum_xi], [sum_xi , sum_xi_sqr]])
+    B = np.array([sum_yi , sum_xi_yi])
+    inv_matrices = inv(A)
+    weights = inv_matrices.dot(B)
+    [c , m ] = weights
+    # y = mx + c
+    print("SLOPE: ", m, "INTERCEPT: ", c)
+
+    X = np.array([np.min(x), np.max(x)])
+    Y = np.zeros(X.shape)
+    Y[0] = m*X[0]+c
+    Y[1] = m*X[1]+c
+
+    plt.scatter(x,y)
+    plt.figure(1)
+    plt.plot(X,Y,'b-')
+    plt.show()
+
+
+    fout = open('dat2.answer' , 'w')
+    result = "SLOPE: "+str(m)+"\nINTERCEPT: "+str(c)+"\n"
+    fout.write(result)
+    fout.close()
