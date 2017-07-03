d891974763b74903467b3e312d85b5dcefa4bc97
Kaushal Kishore <kshr4kshl@gmail.com>
2017-07-03 11:27:35 +0530

    Day 2 Linear Regression

---
+++ day2-programming-assign/linearRegression1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import numpy as np
+from numpy.linalg import inv
+import matplotlib.pyplot as plt
+
+fname = 'dat1.txt'
+data = np.loadtxt(fname)
+x = data[:,0]
+y = data[:,1]
+
+n = int(len(x))
+sum_xi = x.sum()
+sum_xi_sqr = np.square(x).sum()
+sum_yi = y.sum()
+sum_xi_yi = (x*y).sum()
+
+weights = np.array([0 , 0])
+
+
+A = np.array([[n, sum_xi], [sum_xi , sum_xi_sqr]])
+B = np.array([sum_yi , sum_xi_yi])
+inv_matrices = inv(A)
+weights = inv_matrices.dot(B)
+[c , m ] = weights
+# y = mx + c
+print("SLOPE: ", m, "INTERCEPT: ", c)
+
+X = np.array([3 , 10])
+Y = np.zeros(X.shape)
+Y[0] = m*X[0]+c
+Y[1] = m*X[1]+c
+
+plt.scatter(x,y)
+plt.figure(1)
+plt.plot(X,Y,'b-')
+plt.show()
