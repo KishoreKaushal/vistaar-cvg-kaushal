15bf76eb40506416574d55d32b3384770b1c95bc
Kaushal Kishore <kshr4kshl@gmail.com>
2017-07-04 19:55:50 +0530

    Finding Roots

--- day3-programming-assign/rocketEquation.py
+++ day3-programming-assign/rocketEquation.py
@@ -20,10 +20,13 @@ def func(t, v=335.0, u = 2510.0, M0=2.8*1e6 , m = 13.3*1e3, g=9.81):
 mode = int(argv[1])
 
 if(mode == 1):
+    print("Using Bisection Method: ")
     print(bisection(func, 0, 200))
 elif(mode == 2):
+    print("Using Newton-Raphson Method: ")
     print(newtonraphson(func, fPrime, 60))
 elif(mode == 3):
+    print("Using Secant Method: ")
     print(secant(func, [0, 200], 10))
 else:
     print("Undefined Mode NUmber")
